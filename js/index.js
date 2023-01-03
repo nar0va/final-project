e12ec0e451dd6676c85d6ad58c2d87c80a000a0e
nargiz <114987623+nar0va@users.noreply.github.com>
2023-01-04 00:50:16 +0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,41 +1,6 @@
-// function testimonials_hover(){
+function about_modal(){
 
-//     var list = jQuery('.testimonials .testimonials_inner .right .image_list ul li');
-
-//     list.on('mouseenter',function(){
-//         var element = jQuery(this);
-
-//         var elIndex = element.index()+1;
-
-//         list.removeClass('active');
-//         element.addClass('active');
-//         element.closest('.testimonials').find('.quotes ul li').removeClass('active');
-//         element.closest('.testimonials').find('.quotes ul li:nth-child('+elIndex+')').addClass('active');
-//     });
-// }
-
-// testimonials_hover();
-
-// function person2(){
-
-//   // var show = document.getElementsByClassName("person1");
-
-//   $(document).ready(function(){
-//     $("#hide").click(function(){
-//       $("person1").hide();
-//     });
-//     $("#show").click(function(){
-//       $("person2").show();
-//     });
-//   });
-// }
-
-// person2();
-
-
-function waxon_tm_modalbox_about(){
-
-    var modalBox	= jQuery('.waxon_tm_modalbox_about');
+    var modalBox	= jQuery('.about_modal');
     var opener		= jQuery('.about .btnn a');
     var closer		= modalBox.find('.close');
 
@@ -48,32 +13,11 @@ function waxon_tm_modalbox_about(){
         return false;
     });
 }
-waxon_tm_modalbox_about();
+about_modal();
 
 
 
-
-// function tdProgress(container){
-
-//     container.find('.progress_inner').each(function() {
-//         var progress 		= jQuery(this);
-//         var pValue 			= parseInt(progress.data('value'), 10);
-//         var pColor			= progress.data('color');
-//         var pBarWrap 		= progress.find('.bar');
-//         var pBar 			= progress.find('.bar_in');
-//         pBar.css({width:pValue+'%', backgroundColor:pColor});
-//         setTimeout(function(){pBarWrap.addClass('open');});
-//     });
-// }
-
-// jQuery('.waxon_progress').each(function() {
-
-//     var pWrap 			= jQuery(this);
-//     pWrap.waypoint({handler: function(){tdProgress(pWrap);},offset:'90%'});	
-// });
-
-
-function waxon_tm_filter_opener(){
+function filter_open(){
 
     var button	= jQuery('.portfolio .portfolio_filter .wrapper a');
     var list	= jQuery('.portfolio .portfolio_filter ul li');
@@ -93,56 +37,21 @@ function waxon_tm_filter_opener(){
         return false;
     });
 }
-waxon_tm_filter_opener();
+filter_open();
 
 
-function portfolio(){
+function testimonials_hover(){
 
-    if(jQuery().isotope) {
-
-        // Needed variables
-        var list 		 = jQuery('.portfolio .portfolio_inner ul');
-        var filter		 = jQuery('.portfolio .portfolio_filter ul');
-
-        if(filter.length){
-            // Isotope Filter 
-            filter.find('a').on('click', function(){
-                var selector = jQuery(this).attr('data-filter');
-                list.isotope({ 
-                    filter				: selector,
-                    animationOptions	: {
-                        duration			: 750,
-                        easing				: 'linear',
-                        queue				: false
-                    }
-                });
-                return false;
-            });	
-
-            // Change active element class
-            filter.find('a').on('click', function() {
-                filter.find('a').removeClass('current');
-                jQuery(this).addClass('current');
-                return false;
-            });	
-        }
-    }
-}
-portfolio();
-
-
-function waxon_tm_testimonials_effect(){
-
-    var list	= jQuery('.waxon_tm_testimonials .testi_inner .right .image_list ul li');
+    var list	= jQuery('.testimonials .testimonials_inner .right .image_list ul li');
 
     list.on('mouseenter',function(){
         var element = jQuery(this);
         var elIndex = element.index()+1;
         list.removeClass('active');
         element.addClass('active');
-        element.closest('.waxon_tm_testimonials').find('.quote_list ul li').removeClass('active');
-        element.closest('.waxon_tm_testimonials').find('.quote_list ul li:nth-child('+elIndex+')').addClass('active');
+        element.closest('.testimonials').find('.quote_list ul li').removeClass('active');
+        element.closest('.testimonials').find('.quote_list ul li:nth-child('+elIndex+')').addClass('active');
     });
 }
-waxon_tm_testimonials_effect();
+testimonials_hover();
 
